609064415b2548a617e784848dc5f5215b572ac9
janaSunrise <warriordefenderz@gmail.com>
2020-06-17 15:53:55 +0530

    django integration

---
+++ hotwired/hotwired/urls.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
+    path('sudo/', admin.site.urls),
+    path('', include('index.urls'))
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
