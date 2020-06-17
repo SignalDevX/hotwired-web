b3e252ce83c4c07e94ab5c9f1162b21ea9251f97
janaSunrise <warriordefenderz@gmail.com>
2020-06-17 20:20:04 +0530

    fix staticfiles error

--- hotwired/hotwired/settings.py
+++ hotwired/hotwired/settings.py
@@ -118,8 +118,8 @@ X_FRAME_OPTIONS='SAMEORIGIN'
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
