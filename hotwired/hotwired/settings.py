2c68bde5308b162f9b8e4eb55ff6837afe6349f2
janaSunrise <warriordefenderz@gmail.com>
2020-06-17 19:23:50 +0530

    add-changes

--- hotwired/hotwired/settings.py
+++ hotwired/hotwired/settings.py
@@ -115,8 +115,9 @@ X_FRAME_OPTIONS='SAMEORIGIN'
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_files')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
